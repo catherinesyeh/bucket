72cbd6e827cba2cd6b41fd3a70081e01a6306346
22asc4 <asc4@williams.edu>
2020-04-19 17:54:41 -0400

    i attached the script properly this time but it won't click :'(

--- Assets/Intro.cs
+++ Assets/Intro.cs
@@ -6,12 +6,12 @@ using UnityEngine.UI;
 
 public class Intro : MonoBehaviour
 {
-	int slide = 0;
+	int slide = 1;
 
     public void updateText() {
-    	if (slide == 1) {
+    	// if (slide == 1) {
     		GameObject.Find("Message").GetComponent<Text>().text = "WHY IS BKT IMPORTANT?\nBKT is an artificial intelligence algorithm that lets us predict what people know. It is particularly useful in educational settings to help teachers and students measure skill ~mastery~, aka “did you actually learn the stuff I wanted you to learn?” ";
-    	}
+    	// }
     	slide++;
     }
 }
