2887c14a1a16bff7abbabe1a6984f9842ec98771
Catherine :) <43099514+catherinesyeh@users.noreply.github.com>
2020-04-26 12:22:57 -0700

    added back button and text scales now! :)

--- Assets/scripts/Next_Button.cs
+++ Assets/scripts/Next_Button.cs
@@ -6,33 +6,78 @@ using UnityEngine.UI;
 
 public class Next_Button : MonoBehaviour
 {
-	int slide = 1;
+	int slide = 0;
+
+    public Button back;
+    public Button next;
+
+    void Start()
+    {   
+        updateText();
+    }
+
+    public void pressNext()
+    {
+        Debug.Log("Next button pressed");
+        slide++;
+        updateText();
+    }
+
+    public void pressBack()
+    {
+        Debug.Log("Back button pressed");
+        slide--;
+        updateText();
+    }
+
+    public void backToMenu()
+    {
+        Debug.Log("Back to menu");
+        slide = 3;
+        SceneManager.LoadScene("Intro");
+        updateText();
+    }
 
     public void updateText() 
     { 
-        if (slide == 1)
+        if (slide == 0)
+        {
+            GameObject.Find("Message").GetComponent<Text>().text =
+            "Hi! I’m Bucket! Today we’re going to be learning about BKT, or Bayesian Knowledge Tracing!";
+
+            back.gameObject.SetActive(false);
+            next.GetComponentInChildren<Text>().text = "Next";
+        }
+        else if (slide == 1)
         {
             GameObject.Find("Message").GetComponent<Text>().text =
                 "WHY IS BKT IMPORTANT?\nBKT is an artificial intelligence algorithm that lets us predict what people know. " +
                 "It is particularly useful in educational settings to help teachers and students measure skill ~mastery~, " +
                 "aka. \"did you actually learn the stuff I wanted you to learn?\"";
+
+            back.gameObject.SetActive(true);
+            next.GetComponentInChildren<Text>().text = "Next";
         }
         else if (slide == 2)
         {
             GameObject.Find("Message").GetComponent<Text>().text =
                 "Once we’re pretty sure, so like 95-100% sure, that someone knows a thing, we can say they’ve mastered it. " +
                 "As sureness is a probability, you can guess that BKT is just a bunch of probabilities, but stuffed into an algebraic formula.";
+
+            back.gameObject.SetActive(true);
+            next.GetComponentInChildren<Text>().text = "Next";
         }
         else if (slide == 3)
         {
             GameObject.Find("Message").GetComponent<Text>().text =
                 "BKT is built on four probabilities. Click on the buttons below to learn more about them.";
-            GameObject.Find("NextButton").GetComponentInChildren<Text>().text = "P(guess) & P(known)";
+            
+            back.gameObject.SetActive(true);
+            next.GetComponentInChildren<Text>().text = "P(guess) & P(known)";
         }
         else
         { // go to next scene
             SceneManager.LoadScene("P(known)");
         }
-    	slide++;
     }
 }
