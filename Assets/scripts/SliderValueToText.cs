6d4cafa31dbdcde840cc5927201782312f0a4e8d
Catherine :) <43099514+catherinesyeh@users.noreply.github.com>
2020-05-09 20:57:58 -0700

    add text and description for p(guess)

---
+++ Assets/scripts/SliderValueToText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderValueToText : MonoBehaviour
+{
+    public Slider sliderUI;
+    private Text textSliderValue;
+    public float total;
+
+    void Start()
+    {
+        textSliderValue = GetComponent<Text>();
+        total = sliderUI.maxValue;
+    }
+
+    public void OnSliderValueChanged(float num)
+    {
+        string sliderMessage = "Number of Targets = " + num.ToString() + ", P(guess) = " + num.ToString() + "/" + total.ToString();
+        textSliderValue.text = sliderMessage;
+        Debug.Log("Value changed.");
+    }
+}
--- Assets/scripts/Start_Game.cs
+++ Assets/scripts/Start_Game.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,10 +8,14 @@ public class Start_Game : MonoBehaviour
 {
     public Text instructs;
     public Button startBut;
+    public Slider slider;
+    public Text slidertext;
 
     public void StartGame()
     {
         instructs.gameObject.SetActive(false);
         startBut.gameObject.SetActive(false);
+        slider.gameObject.SetActive(false);
+        slidertext.gameObject.SetActive(false);
     }
 }
